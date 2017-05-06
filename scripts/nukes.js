4b081368a75522d72e73fb51eb569ee14d1b7a2e
Adam Akers <adamrakers@gmail.com>
2017-05-05 21:37:58 -0600

    transition theme of site from volcanoes to nukes

---
+++ scripts/nukes.js
@@ -0,0 +1,62 @@
+
+//DC, LA, Austin,
+//Colorado Springs***, Little Rock Arkansas, Honolulu,
+//Seoul
+const cities = [
+  {
+    cityName: 'Colorado Springs, CO',
+    loc: {lat: 38.8339, lng: -104.8214},
+  },
+  {
+    cityName: 'Washington, D.C.',
+    loc: {lat: 38.904722, lon: -77.016389}
+  },
+  {
+    cityName: 'Los Angeles, CA',
+    loc: {lat: 34.05, lon: -118.25}
+  },
+  {
+    cityName: 'Austin, TX',
+    loc: {lat: 30.266667, lon: -97.733333}
+  },
+  {
+    cityName: 'Little Rock, AR',
+    loc: {lat: 34.736111, lon: -92.331111}
+  },
+  {
+    cityName: 'Honolulum HI',
+    loc: {lat: 21.3, lon: -157.816667}
+  },
+  {
+    cityName: 'Seoul, South Korea',
+    loc: {lat: 37.566667, lon: 126.966667}
+  }
+];
+
+const nukes = [
+  {
+    nukeId: 1,
+    date: 'Oct 9, 2006',
+    kt: 0.7
+  },
+  {
+    nukeId: 2,
+    date: 'May 25, 2009',
+    kt: 5.4
+  },
+  {
+    nukeId: 3,
+    date: 'Feb 12, 2013',
+    kt: 14
+  },
+  {
+    nukeId: 4,
+    date: 'Jan 6, 2016',
+    kt: 10
+  },
+  {
+    nukeId: 5,
+    date: 'Sept 9, 2016',
+    kt: 25
+  }
+];
--- scripts/script.js
+++ scripts/script.js
@@ -1,100 +1,40 @@
 
 
 function initMap() {
-  let volcanoes = [
-    {
-      name: 'Mt. Galunggung',
-      location: {lat: 7.256667, lng: 108.076944},
-      date: '1882',
-      deathToll: '4011',
-      vei: '5'
-    },
-    {
-      name: 'Mt. Kelut',
-      location: {lat: 7.93, lng: 112.308},
-      date: '1919',
-      deathToll: '5110',
-      vei: '4'//GOTTA FIND
-    },
-    {
-      name: 'The Laki Volcanic System',
-      location: {lat: 64.064722, lng: -18.226111},
-      date: '1783-1784',
-      deathToll: '9350',
-      vei: '4'//Gotta find
-    },
-    {
-      name: 'Mt. Vesuvius',
-      location: {lat: 40.816667, lng: 14.433333},
-      date: '79 AD',
-      deathToll: '10000+',
-      vei: '5'//Gotta find
-    },
-    {
-      name: 'Mt. Unzen',
-      location: {lat: 32.756667, lng: 130.294444},
-      date: '1792',
-      deathToll: '12000-15000',
-      vei: '3'//Gotta find
-    },
-    {
-      name: 'Nevado Del Ruiz',
-      location: {lat: 4.895278, lng: -75.3225},
-      date: '1985',
-      deathToll: '23000',
-      vei: '3'//Gotta find
-    },
-    {
-      name: 'Mt. Krakatoa',
-      location: {lat: -6.102, lng: 105.423},
-      date: '1883',
-      deathToll: '36000',
-      vei: '6'//Gotta find
-    },
-    {
-      name: 'Mt. Pelee',
-      location: {lat: 14.816667, lng: -61.166667},
-      date: '1883',
-      deathToll: '40000',
-      vei: '4'//Gotta find
-    },
-    {
-      name: 'Mt. Tombora',
-      location: {lat: -8.246667, lng: 117.958333},
-      date: '1816',
-      deathToll: '92000',
-      vei: '7'
-    }    
-  ];
 
+  //Certain coords.
   let mapCenter = {lat: 38.8339, lng: -104.8214};
-  let map = new google.maps.Map(document.getElementById('map'), {
+
+  //Map settings
+  let mapSettings = {
     zoom: 4,
     center: mapCenter
-  });
+  };
 
+  let map = new google.maps.Map(document.getElementById('map'), mapSettings);
 
-  function fillForms(arr, formId) {
+  function citySelectCB() {
+    
+  }
+
+  function fillForms(arr, formId, fn) {
     const ele = document.querySelector(formId);
     arr.forEach( (item, idx) => {
-      let itemStr = `<option value="${idx}">${item.name}</option>`;
+      let strInp = item.cityName ? item.cityName : item.date;
+      let itemStr = `<option value="${idx}">${strInp}</option>`;
       ele.insertAdjacentHTML('beforeend', itemStr);
     });
 
-    ele.addEventListener('input', function(){
-      let volc = volcanoes[this.value];
-      let marker = new google.maps.Marker({
-        center: mapCenter,
-        map: map
-      });
+    ele.addEventListener('input', fn)
+  }
 
-      map.setCenter(volc.location);
-    });
-  };
-  
+  fillForms(cities, '#preset-cities');
+  fillForms(nukes, '#preset-nukes');
 
-  fillForms(volcanoes, '#preset-volcs');
+  console.log(nukes);
 }
+
+
 
 
 
